561036bc2a99e7da6f8acde29296939009676279
Gianluca Valerio <gianluca.valerio1511@gmail.com>
2026-09-19 21:21:18 +0200

    fix base path case

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // IMPORTANTE: sostituisci "libroscambio" con il nome esatto del tuo repo GitHub
-  base: "/schoolbook-market/",
+  base: "/SchoolBook-Market/",
 });
